98b932b70b4dc70d4b9a695ad266c82a0a01c56a
Nismo-Gtr <NCowan3@outlook.com>
2018-06-25 13:40:33 -0500

    Phaser demo deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from "react";
-import app from "./App.css"
+import app from "./App.css";
+import { Loop, Stage } from 'react-game-kit';
 
 
 
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to our Story</h2>
-        </div>
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-      </li>
-        <p className="App-intro">
-          Life is like a game; until you figure it out. 
-        </p>
-        <h2> GAMESOMETHING</h2>
-      </div>
-    );
-  }
+render() {
+  return (
+    <Loop>
+      <Stage>
+        <World>
+          <Body args={[0,0,75,75]} ref={ (b) => this.body = b.body }>
+            // Sprites go here
+          </Body>
+        </World>
+      </Stage>
+    </Loop>
+  );
 }
 
 export default App;
