4c818bc79538df2a42ffac1e892ec8fd45c76755
Dan Fabry <dfabry14@gmail.com>
2018-06-26 14:17:10 -0500

    added back in react elements

---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./jumbotron.css";
+
+const Jumbotron = () => (
+    <div className="size jumbotron jumbotron-fluid">
+        <div className="container">
+            <h1 className="header">The Something Game</h1>
+            <p className="inst">Click play to review coding techniques and principals!</p>
+        </div>
+    </div>
+);
+
+export default Jumbotron;
