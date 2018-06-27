6a4921adbad909de68adca138a9fc0d692dd18a1
Dan Fabry <dfabry14@gmail.com>
2018-06-27 14:26:30 -0500

    replaced repository with React framework

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import Jumbotron from "./components/Jumbotron";
+import Game from "./components/Game";
+
+const App = () => (
+  <div>
+    <NavBar></NavBar>
+    <Jumbotron></Jumbotron>
+    <Game></Game>
+    <Footer></Footer>
+  </div>
+);
+
+
+export default App;
