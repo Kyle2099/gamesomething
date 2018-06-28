16ce44afe9943275bf14346b0ebdf371527d4eef
Dan Fabry <dfabry14@gmail.com>
2018-06-28 10:12:39 -0500

    server and API

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -4,8 +4,8 @@ import "./jumbotron.css";
 const Jumbotron = () => (
     <div className="size jumbotron jumbotron-fluid">
         <div className="container">
-            <h1 className="header">The Something Game</h1>
-            <p className="inst">Click play to review coding techniques and principals!</p>
+            <h1 className="header">Trivia Time!</h1>
+            <p className="inst">Click play to start! Invite your friends for some competition!</p>
         </div>
     </div>
 );
