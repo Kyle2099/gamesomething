3c0e77df7b5b473139bf2df13110f3545eeeda0d
Nismo-Gtr <NCowan3@outlook.com>
2018-06-28 15:38:42 -0500

    Added Countdown Componenet

---
+++ client/src/components/Countdown.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+class Countdown extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {currentCount: 10}
+      }
+      timer() {
+        this.setState({
+          currentCount: this.state.currentCount - 1
+        })
+        if(this.state.currentCount < 1) { 
+          clearInterval(this.intervalId);
+        }
+      }
+      componentDidMount() {
+        this.intervalId = setInterval(this.timer.bind(this), 1000);
+      }
+      componentWillUnmount(){
+        clearInterval(this.intervalId);
+      }
+      render() {
+        return(
+          <div>{this.state.currentCount}</div>
+        );
+      }
+    }
+
+
+
+export default Countdown
