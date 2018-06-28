251c3efcb4c2eb6ce5f5b9f7c595fbd8d2c41cb4
Kyle Souther-Bruno <ksoutherbruno@gmail.com>
2018-06-28 11:20:47 -0500

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
-// import './App.css';
+import './App.css';
+import NavBar from "./components/NavBar";
+import Jumbotron from "./components/Jumbotron";
 import Game from './src/index.js';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <NavBar />
+        <Jumbotron />
       <Game />
       </div>
-    );
+    )
   }
 }
+    
 
 export default App;
