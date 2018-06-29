bd1629411205683a3b6df68ed2ceda500d7ad5df
Nismo-Gtr <NCowan3@outlook.com>
2018-06-29 15:26:36 -0500

    updated questions functionality

--- client/src/components/Questions/question.js
+++ client/src/components/Questions/question.js
@@ -6,9 +6,7 @@ import './question.css'
 
 class Question extends Component {
     state = {
-        questions: [],
-        correctAnswers: [],
-        wrongAnswers: [],
+        questions: null,
         counter: 0,
         allAnswers: [],
         playerScore: 0,
@@ -20,20 +18,26 @@ class Question extends Component {
             .then(res => {
                 console.log(res.data.results)
                 // setState({ questions: res.data.results })
+                const questions = [];
+
                 for (let i = 0; i < 10; i++) {
-                    this.setState({ questions: res.data.results[i].question })
-                    this.setState({ correctAnswers: res.data.results[i].correct_answer })
-                    this.setState({ wrongAnswers: [...res.data.results[i].incorrect_answers] })
+                    questions.push({
+                        question: res.data.results[i].question,
+                        correctAnswers: res.data.results[i].correct_answer,
+                        wrongAnswers: res.data.results[i].incorrect_answers
+                    });
+
                     // console.log("array", this.state.wrongAnswers)
                     // console.log(this.state.correctAnswers)
                     // console.log(this.state.wrongAnswers)
                 }
+                this.setState({ questions });
             })
     }
 
     handleTimeout = () => {
         this.setState({
-            playerWrong: this.state.playerWrong +1,
+            playerWrong: this.state.playerWrong + 1,
             counter: this.state.counter + 1//counter +1 (next question)
         })
     }
@@ -46,7 +50,7 @@ class Question extends Component {
             // console.log("pre score is", this.state.playerScore);
             this.setState({ playerScore: this.state.playerScore + 1 });
         } else {
-            this.setState({ playerWrong: this.state.playerWrong + 1});
+            this.setState({ playerWrong: this.state.playerWrong + 1 });
         }
     }
 
@@ -59,19 +63,14 @@ class Question extends Component {
                     <div className="col s12 m6">
                         <div className="card blue-grey darken-1">
                             <div className="card-content white-text">
-                                <h2><Countdown  handleTimeout={this.handleTimeout}/></h2>
+                                <h2><Countdown handleTimeout={this.handleTimeout} /></h2>
                                 <div>
-                                    {/* {this.state.questions[this.state.counter].question} */}
-                                    {/* <Button type="submit">{this.state.correctAnswers ? {this.state.correctAnswers} : ""}</Button><br />
+                                    {this.state.questions ? this.state.questions[this.state.counter].question : ''}<br /><br />
+                                    <div><Button type="submit">{this.state.questions ? this.state.questions[this.state.counter].correctAnswers : ''}</Button></div>
                                     <br />
-                                    <Button type="submit">{this.state.questions[this.state.count].correct_answer}
-                                    </Button><br />
-                                    <br />
-                                    <Button type="submit">{this.state.questions[this.state.count].wrong_answer}
-                                    </Button><br />
-                                    <br />
-                                    <Button type="submit">{this.state.wrongAnswers[2]}
-                                    </Button><br /> */}
+                                    {this.state.questions ? this.state.questions[this.state.counter].wrongAnswers.map(answer => (
+                                        <div><Button type="submit">{answer}</Button><br /><br /></div>
+                                    )) : ""}
                                     <br />
                                 </div>
                             </div>
